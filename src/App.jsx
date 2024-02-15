770fc3ae86830d1f67dec1770c061c90d0c89e40
Vinícius Silva Santos <vinicius01012@gmail.com>
2024-02-14 22:16:31 -0300

    add component footer
    Adicionei um componente footer, a configuração do prettier e algumas variáveis

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import './assets/styles/typography.scss'
+import './assets/styles/variables.scss'
+import './assets/styles/reset.scss'
+import './assets/styles/normalize.scss'
+import './App.scss'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-
+      <main className='mainContainer'>
+      </main>
+      <Footer/>
     </>
   )
 }
